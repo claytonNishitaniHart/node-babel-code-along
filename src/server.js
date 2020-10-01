7c02146be98828ec1373e096a90ab966131a5771
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2020-10-01 21:59:27 +1300

    add put marketplaces route

--- src/server.js
+++ src/server.js
@@ -47,6 +47,37 @@ server.post('/api/marketplaces', async (req, res) => {
     }
 });
 
+server.put('/api/marketplaces/:id', async (req, res) => {
+    try {
+        const { body } = req;
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ error: 'Marketplace id is required' });
+        }
+
+        if (!body.hasOwnProperty('name') || !body.hasOwnProperty('description') || !body.hasOwnProperty('owner')) {
+            return res.status(400).json({ error: 'Marketplace name, description, owner required' });
+        }
+
+        const marketplace = await Marketplaces.findByIdAndUpdate(id, body, { new: true }).lean();
+        delete marketplace.__v;
+
+        return res.status(200).json({
+            success: true,
+            data: marketplace
+        });
+    } catch (error) {
+        console.error(error);
+
+        if (error.kind == 'ObjectId' && error.path == '_id') {
+            return res.status(400).json({ error: 'Invalid ID parameter' });
+        }
+
+        return res.status(500).send(error);
+    }
+})
+
 server.use('*', (req, res) => {
     return res.status(404).json({ error: 'Route not found' });
 })
